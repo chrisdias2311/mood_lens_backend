f8fa95ac7fc062c71991a79e885026470f119649
Chris <chrisdias2311@gmail.com>
2024-03-16 23:16:21 +0530

    Add functionality to create a new meeting with a random 4-digit code

--- routes/meetings.js
+++ routes/meetings.js
@@ -13,34 +13,46 @@ const StudentReport = require("../schemas/StudentReportSchema");
 
 
 
-
 router.post("/create_meeting", async (req, res) => {
     try {
+        // Generate a random 4-digit code
+        let meet_id = generateRandomCode();
+
+        // Check if a meeting with the same meet_id already exists
+        let existingMeeting = await MeetingReport.findOne({ meet_id: meet_id });
+
+        // If a meeting with the same meet_id exists, generate a new code
+        while (existingMeeting) {
+            meet_id = generateRandomCode();
+            existingMeeting = await MeetingReport.findOne({ meet_id: meet_id });
+        }
+
+        // Create the new meeting
         const newMeeting = new MeetingReport({
-            meet_id: req.body.meet_id,
+            meet_id: meet_id,
             host_id: req.body.host_id,
             title: req.body.title,
             description: req.body.description,
             startTime: req.body.startTime,
-            endTime: "",
+            endTime: req.body.endTime,
             host_name: req.body.host_name
         });
 
-        // save meeting here
-        newMeeting.save()
-            .then(() => {
-                console.log('Meeting created');
-                res.status(200).send(newMeeting);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).send('Error saving meeting');
-            });
+        // Save the new meeting to the database
+        await newMeeting.save();
+
+        // Send the meet_id in the response
+        res.json({ meet_id: meet_id });
     } catch (error) {
-        console.log(error);
-        res.status(500).send('Server error');
+        console.error(error);
+        res.status(500).json({ error: error.message });
     }
 });
+
+function generateRandomCode() {
+    return Math.floor(1000 + Math.random() * 9000); // Generate a random 4-digit number
+}
+
 
 module.exports = router;
 
